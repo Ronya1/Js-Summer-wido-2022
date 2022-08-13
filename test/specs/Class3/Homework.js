b31ee8067abd5febde0b92a212b61f2ed76db462
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-08-12 21:12:56 -0400

    homework v1

--- test/specs/Class3/Homework.js
+++ test/specs/Class3/Homework.js
@@ -1,3 +1,6 @@
+const { expect } = require("chai");
+//const { describe } = require('mocha');
+
 // Due date: Aug 16 eod
 
 /**
@@ -7,11 +10,75 @@
  */
 
 /**
+ * 1.) open darksky.net
+ * 2.) obtain the high temp and store in variable 
+ * 3.) obtain the low temp and store in variable 
+ * 4.) obtain feels like temp and store it in a variable
+ * 5.) verify (to.be.true) that current temp is between high and low 
+ * using - Using class attribute
+ */
+
+describe('feels Like Temp Is Between Low Temp and High Temp', () => {
+    
+    it('Verify Temp', async () => {
+        
+       await browser.url('https://darksky.net/')
+       await browser.pause(1000)
+
+       const feelsLike = await $('.feels-like-text').getText();
+       const highTemp = await $('.high-temp-text').getText();
+       const lowTemp = await $('.low-temp-text').getText();
+       //console.log(feelsLike + "Debug")
+
+        expect (feelsLike <= highTemp && feelsLike >= lowTemp, "Working").to.be.true;
+
+    });
+
+
+});
+
+/**
  * Tc-2:
  * https://www.darsky.net
  * Verify user can get temperature based on zipcode
  * 
  */
+
+/**
+ * GIVEN: 
+ *      I am on https://www.darsky.net
+ * When: 
+ *      I locate the zipCode field and I set the value to my zipcode 
+ * Then:
+ *      I verify I am able to obtain my temp. 
+ */
+
+describe('User is able to get their temp', () => {
+    
+    it.only('ZipCode Temp Check', async() => {
+    await browser.url('https://darksky.net/')
+    await browser.pause(1000)    
+
+    const searchBar = await  $('input[type=text]'); ;
+    await searchBar.setValue('10013');
+    await browser.pause(1000) 
+
+    await $('a[class=searchButton]').click();
+    await browser.pause(2000) 
+
+    expect
+
+//tagName[@attrName='attrValue']
+
+
+
+    });
+
+
+});
+
+
+
 
 /**
  * Tc-3:
