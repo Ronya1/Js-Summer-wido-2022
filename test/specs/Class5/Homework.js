d60b42c38ed07b2abdc347aa6cc1df566be7bd1f
RON - Coding <ron-coding@Ronalds-MBP.lan>
2022-08-18 21:11:06 -0400

    Complete Homework

--- test/specs/Class5/Homework.js
+++ test/specs/Class5/Homework.js
@@ -8,15 +8,18 @@
  * then print array in the console.
  */
 
+const { expect } = require("chai");
+
+// The below code works but I get double the values why??
 describe('Get all timeline data', () => {
   it('Get all Day Data', async () => {
     await browser.url('https://darksky.net/')
     await browser.pause(2000)
 
-    let scroleIntoView = await $('div[class=scroll-container]').scrollIntoView(); 
+    let scroleIntoView = await $('div[class=scroll-container]').scrollIntoView();  
     await browser.pause(2000)
 
-    let allTimeValues = await $$(`//*[@class='hours']`)
+    let allTimeValues = await $$(`//*[@class='hours']`) //*[@class='scroll-container']//*[@class='hours']`
     //console.log(`testing list ${myList}`);
 
     for (const printTimes of allTimeValues) {
@@ -25,10 +28,6 @@ describe('Get all timeline data', () => {
     }
   })  
 
-});
-
-//
-//`//*[@class='scroll-container']//*[@class='hours']`
 
 
 
@@ -58,3 +57,82 @@ describe('Get all timeline data', () => {
  * 8.) Locate and get text for Manila Verify it states Manila 
  * 9.) verify dates iclude aug-20 and sep-5
  */
+
+
+   
+    it.only('Hotels.com', async() => {
+        await browser.url('https://www.hotels.com')
+        await browser.pause(500)
+        let searchBarLocation = await $(`//button[@aria-label="Going to"]`).click()
+        await browser.pause(500)
+        let dropDownSearch = await $(`//input[@placeholder="Going to"]`).setValue('man')
+        await browser.pause(500)
+        let selectManila = await $(`//strong[text()="Manila"]`).click()
+        await browser.pause(500)
+        let dateButtonClick = await $(`//button[@id="date_form_field-btn"]`).click() // clicking on the dates button 
+        await browser.pause(500)
+        // Now need to click back to see August 20th 
+        let dateBackButton = await $(`//button[@data-stid="date-picker-paging"][1]`).click() //Not sure how to do it the other way 
+        await browser.pause(500)
+        // How to select 1 date? //h2[text()="August 2022"] ////button[@aria-label="Aug 20, 2022"]
+        //let fromDateClick = await $(`//button[@aria-label="Aug 20, 2022"]`).click()
+        const allAugFromDates = await $$(`//h2[text()="August 2022"]/following-sibling::table//button[not(@disabled)]`)
+        for (const fromDate of allAugFromDates) {
+            const date = await fromDate.getAttribute('data-day');
+            if (date.localeCompare("20") === 0) {
+                await fromDate.click();
+                break;
+            }
+        }
+        await browser.pause(500)
+
+        const allSeptDates = await $$(`//h2[text()="September 2022"]/following-sibling::table//button[not(@disabled)]`)
+        for (const fromDate2 of allSeptDates) {
+            const date2 = await fromDate2.getAttribute('data-day');
+            if (date2.localeCompare("5") === 0) {
+                await fromDate2.click();
+                break;
+            }
+        }
+            await browser.pause(500)
+            let clickContinueButton = await $(`//button[@class="uitk-button uitk-button-medium uitk-button-has-text uitk-button-primary uitk-layout-flex-item uitk-layout-flex-item-flex-shrink-0 dialog-done"]`).click()
+            await browser.pause(500)
+            let clickSearch = await $(`//button[@id="submit_button"]`).click()
+            console.log("submit clicked dddddddddddd")
+            await browser.pause(500)
+
+            // The below im not sure on 
+            let isManilaPresent = await $(`//button[@data-stid="hotels-destination-menu-trigger"]//span[text()="Going to"]`).isDisplayed('Manila') 
+            console.log(`\n\n Testing Value of ${isManilaPresent}`)
+            await browser.pause(500)
+
+            let isAug20Displayed = await $(`//button[@data-name="startDate"]`).isDisplayed("20") 
+            console.log(`\n\n Testing Value of ${isAug20Displayed}`)
+            await browser.pause(500)
+
+            let isSep5Displayed = await $(`//button[@data-name="endDate"]`).isDisplayed("5") 
+            console.log(`\n\n Testing Value of ${isSep5Displayed}`)
+            await browser.pause(500)
+
+            expect(isManilaPresent, 'Manila is not present').to.be.true 
+            expect(isAug20Displayed, 'Aug 20 is not present').to.be.true
+            expect(isSep5Displayed, 'Sep 5 is not present').to.be.true
+
+        
+// //button[@data-stid="hotels-destination-menu-trigger"]//span[text()="Going to"]
+//span[text()="Going to"
+
+        
+
+        // const allAugDateElements = await $$('//h2[text()="August 2022"]/following-sibling::table//button[not(@disabled)]');
+        // for (const dateElement of allAugDateElements) {
+        //     const date = await dateElement.getAttribute('data-day');
+        //     if (date.localeCompare("20") === 0) {
+        //         await dateElement.click();
+        //         break;
+        //     }
+        // }
+
+})
+
+});
